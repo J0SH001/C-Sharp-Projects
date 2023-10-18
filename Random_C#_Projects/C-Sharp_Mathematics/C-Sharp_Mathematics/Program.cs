1c415539a529cc9489097012443ad4a43ff76173
J0SH001 <gysljosh@gmail.com>
2023-10-17 21:57:58 -0500

    C# math!

---
+++ Random_C#_Projects/C-Sharp_Mathematics/C-Sharp_Mathematics/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Mathematics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 4;
+            
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+
+            int difference = num1 - num2;
+            Console.WriteLine(difference);
+
+            int product = num1 * num2;
+            Console.WriteLine(product);
+
+            int quotient = num1 / num2;
+            Console.WriteLine(quotient);
+
+            double quotient2 = 100.0 / 17.0;
+            Console.WriteLine(quotient2);
+
+            int remainder = 11 % 2;
+            Console.WriteLine(remainder);
+
+
+
+            bool trueOrFalse = 12 < 5;
+            Console.WriteLine(trueOrFalse);
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            bool isWarm = currentTemp >= roomTemp;
+            Console.WriteLine(isWarm);
+
+            bool isEqual = currentTemp == roomTemp;
+            Console.WriteLine(isEqual);
+
+
+            string word1 = "What\'s ";
+            string word2 = "up?";
+            string greeting = word1 + word2;
+            Console.WriteLine(greeting);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to be multiplied by 50");
+            string multiplierText = Console.ReadLine();
+            int multiplier = Convert.ToInt32(multiplierText);
+            int productx = multiplier * 50;
+            Console.WriteLine(productx);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to be increased by 25");
+            string additionText = Console.ReadLine();
+            int addition = Convert.ToInt32(additionText);
+            int additionx = addition + 25;
+            Console.WriteLine(additionx);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to be divided by 12.5");
+            string dividendText = Console.ReadLine();
+            int dividend = Convert.ToInt32(dividendText);
+            double dividendx = dividend / 12.5;
+            Console.WriteLine(dividendx);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to determine if it's greater than 50 or not");
+            string greaterText = Console.ReadLine();
+            int greater = Convert.ToInt32(greaterText);
+            bool greaterx = greater > 50;
+            Console.WriteLine(greaterx);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number to be get the remainder of when divided by 7");
+            string remainderText = Console.ReadLine();
+            int remainder1 = Convert.ToInt32(remainderText);
+            int remainderx = remainder1 % 7;
+            Console.WriteLine(remainderx);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Random_C#_Projects/IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This collects input from user 1 then converts it to int variables
+            Console.WriteLine("Anonymous Income Comparison Program\nPerson 1: Please type your hourly rate and press \"Enter\"");
+            string hourlyRate1 = Console.ReadLine();
+            int rate1 = Convert.ToInt16(hourlyRate1);
+            Console.WriteLine("Person 1: Please type your number of hours per week and press \"Enter\"");
+            string hoursWorked1 = Console.ReadLine();
+            int hours1 = Convert.ToInt16(hoursWorked1);
+
+            //This collects input from user 2 then converts it to int variables
+            Console.WriteLine("Person 2: Please type your hourly rate and press \"Enter\"");
+            string hourlyRate2 = Console.ReadLine();
+            int rate2 = Convert.ToInt16(hourlyRate2);
+            Console.WriteLine("Person 2: Please type your number of hours per week and press \"Enter\"");
+            string hoursWorked2 = Console.ReadLine();
+            int hours2 = Convert.ToInt16(hoursWorked2);
+
+            //This calculates the approximate annual salaries based on the users' input and compares them
+            int salary1 = rate1 * hours1 * 52;
+            int salary2 = rate2 * hours2 * 52;
+            Console.WriteLine("Salary of Person 1 is: $" + salary1);
+            Console.WriteLine("Salary of Person 2 is: $" + salary2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool comparison = salary1 > salary2;
+            Console.WriteLine(comparison);
+            Console.ReadLine();
+        }
+    }
+}
