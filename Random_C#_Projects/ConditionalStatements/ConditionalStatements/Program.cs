4f2c478579e3cbe6d33a4905eec9e82a2af42a7f
J0SH001 <gysljosh@gmail.com>
2023-10-18 14:19:41 -0500

    Branching practice

---
+++ Random_C#_Projects/ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            if (number > 7)
+            {
+                Console.WriteLine("The number is greater than 7");
+            }
+            else if (number <= 2)
+            {
+                Console.WriteLine("The number is less than or equal to 2");
+            }
+            else if (number != 3)
+            {
+                Console.WriteLine("The number is not equal to 3");
+            }
+            else
+            {
+                Console.WriteLine("The number is 3");
+            }
+
+            string numberText = number > 8 ? "The number is greater than 8" : "The number is less than 8";
+            Console.WriteLine(numberText);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Random_C#_Projects/PackageAssignment/PackageAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nWhat is the weight of your package?");
+            int weight = Convert.ToInt16(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("What is the width of your package in inches?");
+                int width = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("What is the height of your package in inches?");
+                int height = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("What is the length of your package in inches?");
+                int length = Convert.ToInt16(Console.ReadLine());
+
+                if (width + height + length > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    double quote = (width * weight * height * length) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote + ".00.\nThank you!");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
