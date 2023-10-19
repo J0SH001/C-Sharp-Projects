ce9cf982023a43239a760dd2322df15eab5d564b
J0SH001 <gysljosh@gmail.com>
2023-10-19 17:54:42 -0500

    This was a bit less confusing

---
+++ Random_C#_Projects/MethodsAssignment2/MethodsAssignment2/Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment2
+{
+    class Numbers
+    {
+        public static int Add(int i)
+        {
+            i = i + 5;
+            return i;
+        }
+        public static int Add(decimal i2)
+        {
+            int newi2 = Convert.ToInt32(i2 - 5);
+            return newi2;
+        }
+
+        public static int Add(string i3)
+        {
+            int newi3 = Convert.ToInt32(i3) * 5;
+            return newi3;
+        }
+    }
+}
---
+++ Random_C#_Projects/MethodsAssignment2/MethodsAssignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number and press \"Enter\".");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            int newNumber = Numbers.Add(i);
+            Console.WriteLine(newNumber);
+
+            Console.WriteLine("Type a decimal number and press \"Enter\".");
+            decimal i2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(Numbers.Add(i2));
+
+            Console.WriteLine("Type a decimal number and press \"Enter\".");
+            string i3 = Console.ReadLine();
+
+            Console.WriteLine(Numbers.Add(i3));
+
+            Console.ReadLine();
+        }
+    }
+}
