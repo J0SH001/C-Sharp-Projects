43923f1e591e067e389de755178355773e813b4e
J0SH001 <gysljosh@gmail.com>
2023-10-19 16:23:53 -0500

    This one was confusing

---
+++ Random_C#_Projects/MethodsAssignment/MethodsAssignment/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Numbers
+    {
+        public static int Add(int i)
+        {
+            i = i + 5;
+            return i;
+        }
+
+        public static int Multiply(int i)
+        {
+            i = i * 5;
+            return i;
+        }
+
+        public static int Subtract(int i)
+        {
+            i = i - 5;
+            return i;
+        }
+        public int i { get; set; }
+    }
+}
---
+++ Random_C#_Projects/MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number and press \"Enter\".");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Numbers.Add(i));
+            Console.WriteLine(Numbers.Multiply(i));
+            Console.WriteLine(Numbers.Subtract(i));
+            Console.ReadLine();
+        }
+    }
+}
