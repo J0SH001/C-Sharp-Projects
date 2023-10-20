4314da73aa7bb134cd6ad3bcc97e7de6a0d0acc7
J0SH001 <gysljosh@gmail.com>
2023-10-19 19:04:11 -0500

    Another Assignment completed

---
+++ Random_C#_Projects/MethodsAssignment3/MethodsAssignment3/Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment3
+{
+    class Numbers
+    {
+        public int Add(int i, string i2 = "1")
+        {
+            int i3 = Convert.ToInt32(i2);
+            int i4 = (i * i3) + 34;
+            return i4;
+        }
+    }
+}
---
+++ Random_C#_Projects/MethodsAssignment3/MethodsAssignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Type in a second number if it strikes your fancy.");
+            string num2 = Console.ReadLine();
+
+            if (num2 == "")
+            {
+                Numbers math = new Numbers();
+                Console.WriteLine(math.Add(num1));
+            }
+            else
+            {
+                Numbers math = new Numbers();
+                Console.WriteLine(math.Add(num1, num2));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
