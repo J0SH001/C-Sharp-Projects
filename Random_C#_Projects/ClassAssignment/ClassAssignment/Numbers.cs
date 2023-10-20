d42b145bf8b102bec3efd3b739ea5a22d60090a5
J0SH001 <gysljosh@gmail.com>
2023-10-20 10:01:31 -0500

    Class Assignment

---
+++ Random_C#_Projects/ClassAssignment/ClassAssignment/Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Numbers
+    {
+        public void Divide(int dividend)
+        {
+            int result = dividend / 2;
+            Console.WriteLine(result);
+        }
+        public void Add(int dividend, out int remainder)
+        {
+            remainder = dividend % 2;
+        }
+
+        public static int Add(int i)
+        {
+            i = i + 5;
+            return i;
+        }
+    }
+}
---
+++ Random_C#_Projects/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type a number and hit \"Enter\".");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Numbers divide = new Numbers();
+            divide.Divide(input);
+            Console.ReadLine();
+
+            int remainder = 0;
+            int input2 = 7;
+            Numbers add = new Numbers();
+            add.Add(input2, out remainder);
+        }
+    }
+}
