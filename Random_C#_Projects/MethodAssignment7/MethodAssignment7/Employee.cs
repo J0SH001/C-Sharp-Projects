57232e94b220636af1965779e8d4d8c14bb14a39
J0SH001 <gysljosh@gmail.com>
2023-10-20 15:00:25 -0500

    MethodAssignment7

---
+++ Random_C#_Projects/MethodAssignment7/MethodAssignment7/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment7
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public static Employee operator== (Employee one, Employee two)
+        {
+            if (one.Id == two.Id)
+            {
+                Console.WriteLine("The Ids are equal!");
+            }
+            else
+            {
+                Console.WriteLine("The Ids are not equal.");
+            }
+            return one;
+        }
+        public static Employee operator!= (Employee one, Employee two)
+        {
+            if (one.Id == two.Id)
+            {
+                Console.WriteLine("The Ids are equal!");
+            }
+            else
+            {
+                Console.WriteLine("The Ids are not equal.");
+            }
+            return one;
+        }
+    }
+}
---
+++ Random_C#_Projects/MethodAssignment7/MethodAssignment7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee();
+            one.FirstName = "First";
+            one.LastName = "Employee";
+            one.Id = 14;
+
+            Employee two = new Employee();
+            two.FirstName = "First";
+            two.LastName = "Employee";
+            two.Id = 13;
+
+            _=one == two;
+            Console.ReadLine();
+        }
+    }
+}
