2cdfee759bfac7b5fcfb12178448b6a08ac1cbf7
J0SH001 <gysljosh@gmail.com>
2023-10-23 17:48:32 -0500

    DateTime Assignment

---
+++ Random_C#_Projects/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Enter a number.");
+            Double number = Convert.ToDouble(Console.ReadLine());
+            DateTime then = now.AddHours(number);
+            Console.WriteLine(then);
+
+            Console.ReadLine();
+        }
+    }
+}
