3b0243a514e5d49d7e848fa4d8bfc719a997cef7
J0SH001 <gysljosh@gmail.com>
2023-11-01 19:53:31 -0500

    Website Submission

---
+++ Scores_Project/Scores/Scores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in yout first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            string path = @"Write the file path here with the file that you want to open at the end";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double tscore = 0.0;
+
+            Console.WriteLine("\nStudent Scores: \n");
+            foreach (string line in lines)
+            {
+                Console.Write("\n" + line);
+                double score = Convert.ToDouble(line);
+                tscore += score;
+            }
+
+            double avgScore = tscore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores. \tAverage score: " + avgScore);
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
